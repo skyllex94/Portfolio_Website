ad08b8033dbf40f8971c3e0d1a1c33b315bce164
skyllex94 <45595147+skyllex94@users.noreply.github.com>
2019-01-19 13:04:39 +0200

    done Contact form

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
 
 const navItems = document.querySelectorAll(".nav-item");
 
